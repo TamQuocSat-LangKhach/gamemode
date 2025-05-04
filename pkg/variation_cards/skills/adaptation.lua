60fa39df0713c7e8b82f99afbbc01cd29fe70022
Mechanel <nyutanislavsky@qq.com>
2025-05-05 01:55:55 +0800

    fix

--- pkg/variation_cards/skills/adaptation.lua
+++ pkg/variation_cards/skills/adaptation.lua
@@ -11,7 +11,7 @@ skill:addEffect("filter", {
   global = true,
   mute = true,
   card_filter = function(self, card, player)
-    return card.name == "adaptation" and player:getMark("adaptation-turn") ~= 0
+    return card and card.name == "adaptation" and player:getMark("adaptation-turn") ~= 0
   end,
   view_as = function(self, player, card)
     return Fk:cloneCard(player:getMark("adaptation-turn"), card.suit, card.number)
@@ -19,23 +19,16 @@ skill:addEffect("filter", {
 })
 
 local spec = {
+  global = true,
   can_refresh = function(self, event, target, player, data)
-    return target == player and (data.card.type == Card.TypeBasic or data.card:isCommonTrick())
+    return target == player and data.card and (data.card.type == Card.TypeBasic or data.card:isCommonTrick())
   end,
   on_refresh = function(self, event, target, player, data)
     player.room:setPlayerMark(player, "adaptation-turn", data.card.name)
   end,
 }
 
-skill:addEffect(fk.PreCardUse, {
-  global = true,
-  can_refresh = spec.can_refresh,
-  on_refresh = spec.on_refresh,
-})
-skill:addEffect(fk.PreCardRespond, {
-  global = true,
-  can_refresh = spec.can_refresh,
-  on_refresh = spec.on_refresh,
-})
+skill:addEffect(fk.PreCardUse, spec)
+skill:addEffect(fk.PreCardRespond, spec)
 
 return skill
